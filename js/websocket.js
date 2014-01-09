6cb775d4c4d707fc31238a902441f5073ac9c99c
Jan Smolka <onkelsmo@gmail.com>
2014-01-09 01:14:13 +0100

    MOD: websocket.js methods

--- js/websocket.js
+++ js/websocket.js
@@ -16,32 +16,92 @@ $(document).ready(function()
 	// connect to a server
 	websocket.onopen = function(ev)
 	{
+		$('#messageBox').append('<div class="systemMessage">Connected!</div>');
 		console.log("Connected to Server");
 	};
 	
-	// close the connection
-	websocket.onclose = function(ev)
+	// send a message
+	$('#sendBtn').click(function()
 	{
-		console.log("Connection closed");
-	};
+		var myMessage = $('#message').val();
+		var myName = $('#name').val();
+		
+		if (myName == "")
+		{
+			alert("Enter your Name first");
+			return;
+		}
+		if (myMessage == "")
+		{
+			alert("Enter some message to chat");
+			return;
+		}
+		
+		// prepare json data
+		var msg = 
+		{
+				message: myMessage,
+				name: myName,
+				color: '<?php echo $colours[$user_colour]; ?>'
+		};
+		
+		// convert and send data to server
+		websocket.send(JSON.stringify(msg));
+	});
 	
 	// message received
 	websocket.onmessage = function(ev)
 	{
-		console.log("Message " + ev.data);
+		var msg = JSON.parse(ev.data);
+		var type = msg.type;
+		var umsg = msg.message;
+		var uname = msg.name;
+		var ucolor = msg.color;
+		
+		if (type == 'usermsg')
+		{
+			$('#messageBox').append
+			(
+				'<div><span class="userName" style="color:#' 
+				+ ucolor +
+				'>' 
+				+ uname +
+				'</span>:<span class="userMessage">' 
+				+ umsg + 
+				'</span></div>'
+			);
+		}
+		if(type == 'system')
+		{
+			$('#messageBox').append
+			(
+				'<div class="systemMsg">'
+				+ umsg +
+				'</div>'
+			);
+		}
+		
+		//console.log("Message " + ev.data);
+		$('#message')val('');
 	};
-	
-	// error
+
 	websocket.onerror = function(ev)
 	{
-		console.log("Error " + ev.data);
-	};
-	
-	// send a message
-	$('#send').click(function()
+		//console.log("Error " + ev.data);
+		$('#messageBox').append
+		(
+				"<div class=\"systemError\">Error Occurred - "
+				+ ev.data +
+				"</div>"
+				);
+		}; 
+	websocket.onclose = function(ev)
 	{
-		var myMessage = 'This is a test message';
-		websocket.send(myMessage);
-	});
+		console.log("Connection closed");
+		$('#message_box').append
+		(
+			"<div class=\"system_msg\">Connection Closed</div>"
+		);
+	}; 
 	
 });
